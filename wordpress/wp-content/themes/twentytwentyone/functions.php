d73ca1b06b03110a5b15a7c861851295544d4a9c
giahuybui <92190458+giahuybui@users.noreply.github.com>
2023-10-24 20:19:56 +0700

    update function

--- wordpress/wp-content/themes/twentytwentyone/functions.php
+++ wordpress/wp-content/themes/twentytwentyone/functions.php
@@ -361,6 +361,19 @@ function twenty_twenty_one_widgets_init() {
 		)
 	);
 
+	// Category module 9
+	register_sidebar(
+		array(
+			'name'          => esc_html__( 'Footer', 'twentytwentyone' ),
+			'id'            => 'sidebar-1',
+			'description'   => esc_html__( 'Add widgets here to appear in your footer.', 'twentytwentyone' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
+
 	// Comment module 8
 	register_sidebar(
 		array(
