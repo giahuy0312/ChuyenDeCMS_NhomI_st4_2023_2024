7efe37d592406e2a234627b6db0d73c238f79776
Nhatleo <120942667+Nhatleo@users.noreply.github.com>
2023-10-25 03:18:24 +0700

    Hoàn thành 14

--- wordpress/wp-content/themes/twentytwentyone/search.php
+++ wordpress/wp-content/themes/twentytwentyone/search.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying search results pages
  *
@@ -11,68 +12,67 @@
 
 get_header();
 
-if ( have_posts() ) {
-	?>
+if (have_posts()) {
+?>
 	<header class="page-header alignwide">
 		<h1 class="page-title">
 			<?php
 			printf(
 				/* translators: %s: Search term. */
-				esc_html__( 'Results for "%s"', 'twentytwentyone' ),
-				'<span class="page-description search-term">' . esc_html( get_search_query() ) . '</span>'
+				esc_html__('Results for "%s"', 'twentytwentyone'),
+				'<span class="page-description search-term">' . esc_html(get_search_query()) . '</span>'
 			);
 			?>
 		</h1>
 	</header><!-- .page-header -->
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
 
-			
-	<div class="search-result-count default-max-width">
-		<?php
-		printf(
-			esc_html(
-				/* translators: %d: The number of search results. */
-				_n(
-					'We found %d result for your search.',
-					'We found %d results for your search.',
-					(int) $wp_query->found_posts,
-					'twentytwentyone'
-				)
-			),
-			(int) $wp_query->found_posts
-		);
-		?>
-	</div><!-- .search-result-count -->
-
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-	<div class="row">
-		<div class="col-md-3">
-			
+			</div>
+			<div class="col-md-4">
+				<div class="search-result-count default-max-width">
+					<?php
+					printf(
+						esc_html(
+							/* translators: %d: The number of search results. */
+							_n(
+								'We found %d result for your search.',
+								'We found %d results for your search.',
+								(int) $wp_query->found_posts,
+								'twentytwentyone'
+							)
+						),
+						(int) $wp_query->found_posts
+					);
+					?>
+				</div><!-- .search-result-count -->
+			</div>
+			<div class="col-md-4">
+			        <?php dynamic_sidebar('comment-14'); ?>
+			</div>
 		</div>
-		<div class="col-md-6">
-			<?php
-			// Start the Loop.
-			while ( have_posts() ) {
-				the_post();
+	</div>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+<?php
+	// Start the Loop.
+	while (have_posts()) {
+		the_post();
 
-				/*
+		/*
 				* Include the Post-Format-specific template for the content.
 				* If you want to override this in a child theme, then include a file
 				* called content-___.php (where ___ is the Post Format name) and that will be used instead.
 				*/
-				get_template_part( 'template-parts/content/content-excerpt', get_post_format() );
-			} // End the loop.
+		get_template_part('template-parts/content/content-excerpt', get_post_format());
+	} // End the loop.
 
-			// Previous/next page navigation.
-			twenty_twenty_one_the_posts_navigation();
+	// Previous/next page navigation.
+	twenty_twenty_one_the_posts_navigation();
 
-			// If no content, include the "No posts found" template.
-		} else {
-			get_template_part( 'template-parts/content/content-none' );
-		}
-		?>
-		</div>
-		<div class="col-md-3">
-			<?php dynamic_sidebar( 'comment-14' ); ?>
-		</div>
-	</div>
+	// If no content, include the "No posts found" template.
+} else {
+	get_template_part('template-parts/content/content-none');
+}
+?>
 <?php get_footer(); ?>
