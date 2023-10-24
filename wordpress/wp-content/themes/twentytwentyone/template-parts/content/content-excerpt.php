8b66163f09fb19d6502f77453dcec764c425aaf5
nham1310 <trannham05042002@gmail.com>
2023-10-24 19:23:55 +0700

    ddd

--- wordpress/wp-content/themes/twentytwentyone/template-parts/content/content-excerpt.php
+++ wordpress/wp-content/themes/twentytwentyone/template-parts/content/content-excerpt.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template part for displaying post archives and search results
  *
@@ -13,12 +14,11 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<?php get_template_part( 'template-parts/header/excerpt-header', get_post_format() ); ?>
+	<?php get_template_part('template-parts/header/excerpt-header',  dynamic_sidebar('content'))  ?>
 
 	<div class="entry-content">
-		<?php get_template_part( 'template-parts/excerpt/excerpt', get_post_format() ); ?>
+		<?php get_template_part('template-parts/excerpt/excerpt', dynamic_sidebar('content')); ?>
 	</div><!-- .entry-content -->
-
 	<footer class="entry-footer default-max-width">
 		<?php twenty_twenty_one_entry_meta_footer(); ?>
 	</footer><!-- .entry-footer -->
